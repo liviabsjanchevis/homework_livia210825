f7e2c646e75f138401e3f550398ad12efd1be16e
liviabsjanchevis <88117484+liviabsjanchevis@users.noreply.github.com>
2021-08-25 20:36:55 -0300

    Add files via upload

---
+++ livia_210820.py
@@ -0,0 +1,35 @@
+print("Question2**************")
+for phone in range(30,41):
+    print(phone)
+
+print("Question3**************")
+for phone in range(30,41):
+    if phone < 35:
+        print(phone)
+
+print("Question4****************")
+for phone in range(30,41):
+    if phone <= 35:
+        print(phone)
+
+print("Question5***************")
+for phone in range(30,41):
+    if phone > 35:
+        print(phone)
+
+print("Question6******************")
+for phone in range(30,41):
+    if phone >= 35:
+        print(phone)
+
+print("Question7******************")
+for phone in range(30,41):
+    if phone < 33:
+        print(phone)
+
+print("Question8*****************")
+for phone in range(30,41):
+    if phone < 33:
+        print(phone)
+
+#I can't do the question 7 and 8, because i didn't remenber...
